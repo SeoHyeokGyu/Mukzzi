e8ba2dfd7aba7cdd7abc900bee027f0ce03817fb
seohyeokgyu <shk64099@gmail.com>
2026-04-03 21:40:49 +0900

    refactor: DB 마이그레이션 제거 (수동 생성으로 전환)

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -7,7 +7,6 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 
-	"github.com/SeoHyeokGyu/Mukzzi/backend/internal/domain"
 	"github.com/SeoHyeokGyu/Mukzzi/backend/internal/handler"
 	"github.com/SeoHyeokGyu/Mukzzi/backend/internal/repository"
 	"github.com/SeoHyeokGyu/Mukzzi/backend/internal/router"
@@ -37,11 +36,6 @@ func main() {
 	db, err := gorm.Open(postgres.Open(databaseURL), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
-	}
-
-	// DB 마이그레이션
-	if err := db.AutoMigrate(&domain.Badge{}, &domain.UserBadge{}); err != nil {
-		log.Fatalf("Failed to run migrations: %v", err)
 	}
 
 	// 의존성 조립 (DI)
