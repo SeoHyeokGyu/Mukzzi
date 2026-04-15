a23d59bb5221be2eda3dd027d548509e9a6d0b22
seohyeokgyu <shk64099@gmail.com>
2026-04-15 20:32:50 +0900

    fix: GetByID에서 NutritionGoal Preload 제거 (user_nutrition_goals 테이블 미존재)

--- backend/internal/repository/user.go
+++ backend/internal/repository/user.go
@@ -40,7 +40,7 @@ func (r *userRepository) Create(user *domain.User) error {
 
 func (r *userRepository) GetByID(id int64) (*domain.User, error) {
 	var user domain.User
-	err := r.db.Preload("NutritionGoal").First(&user, id).Error
+	err := r.db.First(&user, id).Error
 	if err != nil {
 		return nil, err
 	}
