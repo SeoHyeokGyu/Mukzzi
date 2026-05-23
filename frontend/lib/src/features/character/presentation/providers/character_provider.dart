bc5d50c5a018c3699ab7c3fa2dd67bdb942a357c
seohyeokgyu <shk64099@gmail.com>
2026-05-23 11:40:46 +0900

    refactor: CharacterVariantNotifier 및 characterVariantProvider 제거

--- frontend/lib/src/features/character/presentation/providers/character_provider.dart
+++ frontend/lib/src/features/character/presentation/providers/character_provider.dart
@@ -1,8 +1,4 @@
-import 'dart:async' show unawaited;
-
-import 'package:flutter/foundation.dart' show debugPrint;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import '../../../../core/providers/common_providers.dart';
 import '../../../../core/widgets/mukzzi_character.dart';
 import '../../data/models/character_model.dart';
@@ -40,43 +36,4 @@ class TestCharacterNotifier extends StateNotifier<AsyncValue<CharacterModel?>> {
 
 final testCharacterProvider = StateNotifierProvider.autoDispose<TestCharacterNotifier, AsyncValue<CharacterModel?>>((ref) {
   return TestCharacterNotifier(ref);
-});
-
-class CharacterVariantNotifier extends StateNotifier<CharacterVariant> {
-  CharacterVariantNotifier() : super(CharacterVariant.v1) {
-    unawaited(_load());
-  }
-
-  static const _prefsKey = 'character_variant';
-  bool _userSelected = false;
-
-  Future<void> _load() async {
-    try {
-      final prefs = await SharedPreferences.getInstance();
-      final saved = prefs.getString(_prefsKey);
-      if (saved != null && !_userSelected) {
-        state = CharacterVariant.values.firstWhere(
-          (v) => v.name == saved,
-          orElse: () => CharacterVariant.v1,
-        );
-      }
-    } on Exception catch (e) {
-      debugPrint('CharacterVariantNotifier: failed to load preference: $e');
-    }
-  }
-
-  Future<void> setVariant(CharacterVariant variant) async {
-    _userSelected = true;
-    state = variant;
-    try {
-      final prefs = await SharedPreferences.getInstance();
-      await prefs.setString(_prefsKey, variant.name);
-    } on Exception catch (e) {
-      debugPrint('CharacterVariantNotifier: failed to save preference: $e');
-    }
-  }
-}
-
-final characterVariantProvider = StateNotifierProvider<CharacterVariantNotifier, CharacterVariant>((ref) {
-  return CharacterVariantNotifier();
 });
